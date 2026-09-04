a976e3d092f557cc0c5515e69bc9b21ee2cfb79a
Aruzhan Zharkenova <Aruzhan.Zharkenova@dbt.asia>
2026-09-04 14:06:54 +0500

    #18 Add Edit Wish Action

--- src/app/features/wishes/components/wish-card/wish-card.ts
+++ src/app/features/wishes/components/wish-card/wish-card.ts
@@ -11,8 +11,13 @@ import { WishResponse } from '../../../../core/models/wish.model';
 export class WishCard {
   readonly wish = input.required<WishResponse>();
   readonly deleteWish = output<number>();
+  readonly editWish = output<WishResponse>();
 
   protected requestDelete(): void {
     this.deleteWish.emit(this.wish().id);
+  }
+
+  protected requestEdit(): void {
+    this.editWish.emit(this.wish());
   }
 }
